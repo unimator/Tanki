01a7d557cc50493f644cc58595349acb7b5cdb6f
Marcin Górka <unimator@hotmail.com>
2017-09-15 00:04:43 +0200

    tank animation

--- Tanki_UnityProject/Assets/Scripts/Level/Tank.cs
+++ Tanki_UnityProject/Assets/Scripts/Level/Tank.cs
@@ -1,19 +1,22 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 
 public class Tank : MonoBehaviour
 {
     public float MovementSpeed;
+    public Color TankColor;
 
     private LookingDirection _lookingDirection;
+
+    public event EventHandler OnMove, OnMoveStop;
     
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+	    PaintTank();
 	}
-	
-	// Update is called once per frame
+
+    // Update is called once per frame
 	void Update ()
 	{
 	    HandleKeyboardInput();
@@ -26,31 +29,50 @@ public class Tank : MonoBehaviour
         var z = transform.position.z;
         var rotation = transform.rotation;
 
+        bool moved = false;
+
         if (Input.GetKey(KeyCode.UpArrow))
         {
             _lookingDirection = LookingDirection.Forward;
             y += MovementSpeed;
             rotation = Quaternion.AngleAxis(0, Vector3.forward);
+            moved = true;
         }
         else if (Input.GetKey(KeyCode.DownArrow))
         {
             _lookingDirection = LookingDirection.Backward;
             y -= MovementSpeed;
             rotation = Quaternion.AngleAxis(180, Vector3.forward);
+            moved = true;
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
             _lookingDirection = LookingDirection.Right;
             x += MovementSpeed;
             rotation = Quaternion.AngleAxis(270, Vector3.forward);
+            moved = true;
         } 
         else if (Input.GetKey(KeyCode.LeftArrow))
         {
             _lookingDirection = LookingDirection.Left;
             x -= MovementSpeed;
             rotation = Quaternion.AngleAxis(90, Vector3.forward);
+            moved = true;
         }
 
+        if (moved && OnMove != null)
+            OnMove(this, new EventArgs());
+        else if (OnMoveStop != null)
+            OnMoveStop(this, new EventArgs());
+
         this.transform.SetPositionAndRotation(new Vector3(x, y, z), rotation);
+    }
+
+    private void PaintTank()
+    {
+        foreach (var childSpriteRenderer in GetComponentsInChildren<SpriteRenderer>())
+        {
+            childSpriteRenderer.color = TankColor;
+        }
     }
 }
---
+++ Tanki_UnityProject/Assets/Scripts/Level/TracksAnimation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TracksAnimation : MonoBehaviour
+{
+	// Use this for initialization
+	void Start ()
+	{
+	    var animator = transform.GetComponent<Animator>();
+	    var tank = transform.parent.GetComponent<Tank>();
+
+	    if (tank != null)
+	    {
+	        tank.OnMove += (o, e) =>
+	        {
+                animator.Play("TracksMovement");
+	        };
+
+	        tank.OnMoveStop += (o, e) =>
+	        {
+                animator.Play("Idle");
+	        };
+	    }
+	}
+}
